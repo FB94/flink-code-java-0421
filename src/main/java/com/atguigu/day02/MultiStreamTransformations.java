dd21704a789c6881b9cab516fdfa3455e14c592f
FengBing <a1282558666@163.com>
2020-09-27 19:48:34 +0800

    update

---
+++ src/main/java/com/atguigu/day02/MultiStreamTransformations.java
@@ -0,0 +1,44 @@
+package com.atguigu.day02;
+
+import com.atguigu.day02.util.*;
+import com.atguigu.day02.util.SmokeLevelSource;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.CoFlatMapFunction;
+import org.apache.flink.util.Collector;
+
+public class MultiStreamTransformations {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        DataStreamSource<SensorReading> tempReadings = env.addSource(new SensorSource());
+        DataStreamSource<SmokeLevel> smokeReadings = env.addSource(new SmokeLevelSource()).setParallelism(1);
+
+        tempReadings.keyBy(r -> r.id)
+                .connect(smokeReadings.broadcast())
+                .flatMap(new RaiseAlertFlatMap())
+                .print();
+        env.execute();
+
+    }
+
+    public static class RaiseAlertFlatMap implements CoFlatMapFunction<SensorReading, SmokeLevel, Alert> {
+        private SmokeLevel smokeLevel = SmokeLevel.LOW;
+
+        @Override
+        public void flatMap1(SensorReading sensorReading, Collector<Alert> collector) throws Exception {
+            if (this.smokeLevel == SmokeLevel.HIGH && sensorReading.temperature > 0.0) {
+                collector.collect(new Alert("报警！！" + sensorReading, sensorReading.timestamp));
+            }
+
+        }
+
+
+        @Override
+        public void flatMap2(SmokeLevel smokeLevel, Collector<Alert> collector) throws Exception {
+            this.smokeLevel = smokeLevel;
+
+        }
+    }
+}
